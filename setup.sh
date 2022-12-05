607c540e0ff456521c1bab05c3489c99d4e75c1d
IncroyablePix <18048162+IncroyablePix@users.noreply.github.com>
2022-12-05 11:43:36 +0100

    Fixed setup script (x2)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export LDFLAGS=-L/usr/local/opt/LoRaDraginoDriver/lib
-export CPPFLAGS=-I/usr/local/opt/LoRaDraginoDriver/include
+export CPPFLAGS=-I/usr/local/opt/LoRaDraginoDriver/include/LoRaDraginoDriver
 
 mkdir -p /usr/local/opt/LoRaDraginoDriver/lib
 mkdir -p /usr/local/opt/LoRaDraginoDriver/include/LoRaDraginoDriver
